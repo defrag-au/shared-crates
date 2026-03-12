90980428145bce8886430636c554fd61362b0a44
Damon Oehlman <damon.oehlman@gmail.com>
2026-03-13 09:02:45 +1000

    screenshot tool

--- ui/egui-widgets/src/lib.rs
+++ ui/egui-widgets/src/lib.rs
@@ -1,6 +1,8 @@
 pub mod image_loader;
 pub mod listing_grid;
+pub mod screenshot;
 #[cfg(target_arch = "wasm32")]
 pub mod wallet;
 
 pub use listing_grid::{ListingCard, ListingGrid, ListingGridConfig};
+pub use screenshot::ScreenshotButton;
